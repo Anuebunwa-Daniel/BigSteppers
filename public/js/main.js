496f09bd6f505bfa6f9a5a3481027f73e2a25cf3
DANIEL <107651766+Anuebunwa-Daniel@users.noreply.github.com>
2023-08-31 22:35:50 +0100

    Bigstepper Hotels

---
+++ public/js/main.js
@@ -0,0 +1,64 @@
+ 
+  $('.top').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
+
+  
+  $('.room').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+  })
+
+  $('.gal').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    nav:false,
+    margin:30,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+  })
+
+
+  if ($("[data-fancybox]").length) {
+    $("[data-fancybox]").fancybox();
+}
